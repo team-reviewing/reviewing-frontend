580403b8730c5e8d2e3a4b446412bb67fd560661
leeuihyun <88185154+leeuihyun@users.noreply.github.com>
2023-04-03 19:07:51 +0900

    [#41] 리뷰 수락 및 거절 기능 구현 (#44)
    * feat : 리뷰어 - 리뷰 거절, 수락 Api 추가
    
    * feat : 리뷰 거절 수락 비동기 요청 함수 작성
    
    * feat : 리뷰 요청, 수락 react-query 구현
    
    * refactor : 타입 수정, 이벤트 작성

--- components/ButtonWrapper.tsx
+++ components/ButtonWrapper.tsx
@@ -1,11 +1,13 @@
 interface IButtonProps {
   children: React.ReactNode;
   option?: string;
+  onClick?: () => void;
 }
 
-function ButtonWrapper({ children, option }: IButtonProps) {
+function ButtonWrapper({ children, option, onClick }: IButtonProps) {
   return (
     <button
+      onClick={onClick}
       className={`w-full flex justify-center items-center bg-black text-white h-10 rounded-md ${option ? option : ''}`}>
       {children}
     </button>
--- components/ReviewListSearch/queries/getReviewsQuery.tsx
+++ components/ReviewListSearch/queries/getReviewsQuery.tsx
@@ -1,5 +1,8 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { getReviewsRole } from '../../../pages/api/inquire';
+import { IReviewDetailInfoApiPropsType } from '../../ReviewModal/reviewModalType';
+import { acceptReview, refuseReview } from '../../../pages/api/inquire';
+import { toast } from 'react-hot-toast';
 
 const ROLE = 'REVIEWS';
 
@@ -8,5 +11,33 @@ export function useGetRoleReviews(role: boolean) {
   return useQuery(['getReviews', ROLE, stringRole], () => getReviewsRole(role), {
     retry: false,
     staleTime: 1000 * 20,
+  });
+}
+
+export function useAcceptReview({ reviewerId, reviewId }: IReviewDetailInfoApiPropsType) {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: () => acceptReview({ reviewerId: reviewerId, reviewId: reviewId }),
+    onSuccess: () => {
+      toast.success('리뷰를 수락했습니다');
+      queryClient.invalidateQueries(['getReviews', ROLE, 'reviewer']);
+    },
+    onError: () => {
+      toast.error('오류가 발생했습니다.');
+    },
+  });
+}
+
+export function useRefuseReview({ reviewerId, reviewId }: IReviewDetailInfoApiPropsType) {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: () => refuseReview({ reviewerId: reviewerId, reviewId: reviewId }),
+    onSuccess: () => {
+      toast.success('리뷰를 거절했습니다');
+      queryClient.invalidateQueries(['getReviews', ROLE, 'reviewer']);
+    },
+    onError: () => {
+      toast.error('오류가 발생했습니다.');
+    },
   });
 }
