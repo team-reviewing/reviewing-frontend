b6416333475239d9ac93b7f6e846385c2486e4aa
leeuihyun <meta212121@naver.com>
2023-04-10 16:15:30 +0900

    refactor : Index getServerSideProps get Tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,27 @@
+import { GetServerSideProps } from 'next';
 import Template from '../components/Main/Template';
+import { ICategoriesType } from '../components/Main/mainType';
+import axios from 'axios';
 
-const Home = () => {
+const Home = ({ categories }: ICategoriesType) => {
   return (
     <>
       <div className="w-full h-full flex justify-center">
-        <Template />
+        <Template categories={categories} />
       </div>
     </>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const response = await axios.get('http://localhost:3000/tags');
+  const categories = response.data.categories;
+
+  return {
+    props: {
+      categories,
+    },
+  };
 };
 
 export default Home;
