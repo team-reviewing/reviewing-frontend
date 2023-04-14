07f41f59fc57ac672c3b611424a5a4ef068df968
leeuihyun <88185154+leeuihyun@users.noreply.github.com>
2023-04-14 15:36:57 +0900

    refactor : Index Page SSR 내에서 msw 서버 실행 수정 (#67)

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import { GetServerSideProps } from 'next';
 import Template from '../components/Main/Template';
 import { ICategoriesType } from '../components/Main/mainType';
 import { getCategories } from './api/main';
+import { setupServer } from 'msw/node';
+import { handlers } from '../mocks/handlers';
 
 const Home = ({ categories }: ICategoriesType) => {
   return (
@@ -14,6 +16,9 @@ const Home = ({ categories }: ICategoriesType) => {
 };
 
 export const getServerSideProps: GetServerSideProps = async () => {
+  if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
+    setupServer(...handlers);
+  }
   const response = await getCategories();
   const categories = response.data.categories;
 
