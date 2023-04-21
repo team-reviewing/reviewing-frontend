d0954b346bf00d8408d61a1046fcaf77017ab444
kuku <covernat6949@gmail.com>
2023-04-21 17:46:33 +0900

    refactor : components 정의 축약어로 변경

--- components/Commons/CarouselCard.tsx
+++ components/Commons/CarouselCard.tsx
@@ -2,7 +2,7 @@ import { StrictPropsWithChildren } from './commonsType';
 
 function CarouselCard({ children }: StrictPropsWithChildren) {
   return (
-    <div className="flex items-center justify-between h-full mx-auto max-w-7xl bg-carouselBg">
+    <div className="h-full mx-auto flex-cc max-w-7xl bg-carouselBg">
       <div className="flex justify-center w-full text-4xl font-bold leading-10 tracking-widest ">
         <div className="w-full max-w-3xl flex-cc-col">
           <span className="text-2xl text-center sm:text-2xl md:text-3xl lg:text-4xl text-neutral100 ">{children}</span>
