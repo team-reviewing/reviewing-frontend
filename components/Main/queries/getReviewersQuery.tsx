7bdcf8ddac2e6208d9fb1fb7fbba52c2fed66ec5
kuku <covernat6949@gmail.com>
2023-04-26 12:40:07 +0900

    refactor : 자기 자신은 리뷰어 목록에 나오지 않도록 수정

--- components/Main/queries/getReviewersQuery.tsx
+++ components/Main/queries/getReviewersQuery.tsx
@@ -1,11 +1,14 @@
 import { useInfiniteQuery } from '@tanstack/react-query';
 import { toast } from 'react-hot-toast';
+import { useRecoilValue } from 'recoil';
+import { userState } from '../../../atoms/userState';
 import { getReviewers } from '../../../pages/api/main';
 import { IReviewersRequestType, IReviewerQueryType } from '../mainType';
 
 const REVIEWER_KEY = 'reviewer';
 
 function useGetReviewers(params: IReviewersRequestType, options: IReviewerQueryType) {
+  const user = useRecoilValue(userState);
   return useInfiniteQuery(
     [REVIEWER_KEY, params],
     ({ pageParam = 0 }) => getReviewers({ size: 9, page: pageParam, ...params }),
@@ -14,6 +17,14 @@ function useGetReviewers(params: IReviewersRequestType, options: IReviewerQueryT
       staleTime: 1000 * 20,
       onError: () => {
         toast.error('리뷰어 요청에 실패했습니다');
+      },
+      select: (num) => {
+        return {
+          ...num,
+          pages: num.pages.map((v) => {
+            return { ...v, reviewers: v.reviewers.filter((v2) => v2.username !== user?.username) };
+          }),
+        };
       },
       ...options,
     },
