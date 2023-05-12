3f701d1b7790bb6a60347cb2ae8badf4e59d28af
leeuihyun <meta212121@naver.com>
2023-05-12 19:30:11 +0900

    feat : 리뷰어 상세 정보 모달 타입 관리 파일

---
+++ components/ReviewerModal/reviewerModalType.ts
@@ -0,0 +1,32 @@
+import { IReviewerType } from '../Main/mainType';
+
+export interface IReviewerModalResponseType extends IReviewerType {
+  score: number;
+}
+
+export interface IReviewerScoreListType {
+  page: number;
+  size: number;
+}
+
+export interface IReviewerScoreListResponseType {
+  evaluations: IEvaluationsType[];
+}
+
+export interface IEvaluationsType {
+  id: number;
+  username: string;
+  imageUrl: string;
+  score: number;
+  content: string;
+}
+
+export interface IReviewerModalType {
+  id: number;
+  closeModal: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export interface IReviewerPaginationButtonType {
+  handler: React.MouseEventHandler<HTMLButtonElement>;
+  children: React.ReactNode;
+}
