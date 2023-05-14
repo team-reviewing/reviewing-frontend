760f55fb8a8338ab3bac4d78dc8238abef564113
kuku <covernat6949@gmail.com>
2023-05-14 17:45:08 +0900

    feat : 리뷰 평가 header 추가

---
+++ components/ReviewRate/ReviewRateHeader.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image';
+import cancel from '../../styles/images/cancel.svg';
+import { IReviewRateModalProps } from './reviewRateType';
+
+function ReviewRateHeader({ closeHandler }: Pick<IReviewRateModalProps, 'closeHandler'>) {
+  return (
+    <figure className="flex justify-end">
+      <Image src={cancel} alt={'modalOFF'} width="15" height="15" className="cursor-pointer" onClick={closeHandler} />
+    </figure>
+  );
+}
+
+export default ReviewRateHeader;
