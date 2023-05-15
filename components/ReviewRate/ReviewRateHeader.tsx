a6b0de988d641434798302ee3dd970d1e78ac5a3
kuku <covernat6949@gmail.com>
2023-05-15 17:20:12 +0900

    refactor : Image alt 불피요한 코드 제거

--- components/ReviewRate/ReviewRateHeader.tsx
+++ components/ReviewRate/ReviewRateHeader.tsx
@@ -5,7 +5,7 @@ import { IReviewRateModalProps } from './reviewRateType';
 function ReviewRateHeader({ closeHandler }: Pick<IReviewRateModalProps, 'closeHandler'>) {
   return (
     <figure className="flex justify-end">
-      <Image src={cancel} alt={'modalOFF'} width="15" height="15" className="cursor-pointer" onClick={closeHandler} />
+      <Image src={cancel} alt="modalOFF" width="15" height="15" className="cursor-pointer" onClick={closeHandler} />
     </figure>
   );
 }
