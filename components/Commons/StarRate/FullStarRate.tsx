21d423e8996bdde47e1dce99b8cea3ec75b9e32b
leeuihyun <meta212121@naver.com>
2023-06-05 13:50:54 +0900

    refactor : 불필요한 변수 삭제(StarRate)

--- components/Commons/StarRate/FullStarRate.tsx
+++ components/Commons/StarRate/FullStarRate.tsx
@@ -1,9 +1,8 @@
 import { ISingleStarRatingPropsType } from './starRateType';
 
-function FullStarRate({ fillColor = '#ffc342', size }: ISingleStarRatingPropsType) {
-  const height = size ? size : 40;
+function FullStarRate({ fillColor = '#ffc342', size = 40 }: ISingleStarRatingPropsType) {
   return (
-    <svg height={height} viewBox="0 0 24 24">
+    <svg height={size} viewBox="0 0 24 24">
       <path
         d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"
         fill={fillColor}
